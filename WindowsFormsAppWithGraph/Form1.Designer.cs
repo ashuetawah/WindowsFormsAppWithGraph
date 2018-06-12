7a6611755946b6e5d741b044114e77d5edf13be1
Ashish Trivedi <ashuetawah@hotmail.com>
2018-06-12 20:11:50 +1000

    Project initial commit

---
+++ WindowsFormsAppWithGraph/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace WindowsFormsAppWithGraph
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.graphLoginComponent1 = new Microsoft.Toolkit.Services.WinForms.GraphLoginComponent(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // graphLoginComponent1
+            // 
+            this.graphLoginComponent1.ClientId = null;
+            this.graphLoginComponent1.Scopes = null;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(120, 251);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(152, 46);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(115, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(115, 130);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(746, 66);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(297, 262);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(120, 465);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 33;
+            this.dataGridView1.Size = new System.Drawing.Size(923, 552);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1099, 1090);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Microsoft.Toolkit.Services.WinForms.GraphLoginComponent graphLoginComponent1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ WindowsFormsAppWithGraph/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Microsoft.Toolkit.Services.WinForms;
+using Microsoft.Toolkit.Services.Services.MicrosoftGraph;
+
+namespace WindowsFormsAppWithGraph
+{
+    public partial class Form1 : Form
+    {
+        private Microsoft.Graph.GraphServiceClient graphClient = null;
+        public Form1()
+        {
+            InitializeComponent();
+            graphLoginComponent1.ClientId = "<Your Application ID>";
+            graphLoginComponent1.Scopes = new string[] { MicrosoftGraphScope.UserRead, MicrosoftGraphScope.FilesReadAll };
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            if (!await graphLoginComponent1.LoginAsync())
+            {
+                return;
+            }
+            //update the user's display fields
+            label1.Text = graphLoginComponent1.DisplayName;
+            label2.Text = graphLoginComponent1.JobTitle;
+            pictureBox1.Image = graphLoginComponent1.Photo;
+            // Do more things with the graph
+            graphClient = graphLoginComponent1.GraphServiceClient;
+            var rootItems = await graphClient.Me.Drive.Root.Children.Request().GetAsync();
+            BindingSource bindingSource1 = new BindingSource();
+            dataGridView1.DataSource = bindingSource1;
+            bindingSource1.DataSource = rootItems;
+            dataGridView1.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCellsExceptHeader);
+        }
+    }
+}
